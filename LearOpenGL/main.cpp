14642af9eb9aa8ce10b99644909efe4745f28637
Dongfang334425 <dongfang334425@163.com>
2020-11-23 11:54:33 +0800

    Create Hello-Triangle

--- LearOpenGL/main.cpp
+++ LearOpenGL/main.cpp
@@ -11,6 +11,34 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+/*DF: The vertex shader is one of the shaders that are programmable by people like us.
+Modern OpenGL requires that we at least set up a vertex and fragment shader if we want 
+to do some rendering.
+The first thing we need to do is write the vertex shader in the shader language GLSL(OpenGL
+Shading Language) and then compile this shader so we can use if in our application.
+
+GLSL:
+each shader begins with a declaration of its version.
+next we declare all the input vertex attributes in the vertex shader with the "in" keyword.
+Right now we only care about position data so we only need a single vertex attribute.
+GLSL has a vector datatype that contains 1 to 4 floats based on its postfix digit. Since each
+vertex has a 3D coordinate we create a "vec3" input variable with the name aPos. We also specifically
+set the location of the input variable via layout (location = 0).
+*/
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor=vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\n\0";
+
 int main()
 {
 	
@@ -49,6 +77,81 @@ int main()
 		return -1;
 	}
 
+
+	//build and compile our shader program
+	//------------------------------------
+	//vertex shader
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	/*DF: glShaderSource function takes the shader object to compile to as its first argument.
+	The second argument specifies how many strings we're passing as source code, which is only one.
+	The third parameter is the actual source code of the vertex shader and we can leave the 4th parameter
+	to NULL.*/
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+	}
+	//fragment shader
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	//check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+	}
+	//link shaders
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//set up vertex data (and buffer(s)) and configure vertex attributes
+	//------------------------------------------------------------------
+	float vertices[]=
+	{
+		-0.5f,-0.5f,0.0f,//left
+		0.5f,-0.5f,0.0f,//right
+		0.0f,0.5f,0.0f //top
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);//VAO: vertex array object???
+	glGenBuffers(1, &VBO);
+	//Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure
+	//vertex attributes(s);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -57,11 +160,23 @@ int main()
 		// -----
 		processInput(window);
 
+		//the graphics pipeline can be divided into several steps where each step requires the output of the previous
+		//as its input.
+		//All of these steps are highly specialized (they have one specific function) and can easily be executed 
+		//in parallel.
+
+
+
 		// render
 		// ------
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);/*DF: clear the screen's color buffer using glClear where we pass in buffer bits to 
 									 specify which buffer we would like to clear.*/
+									 // draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
@@ -71,6 +186,11 @@ int main()
 		glfwPollEvents();/*DF: check if any events are triggered like keyboard input or mouse movement events.
 						 updates the window state, and calls the corresponding functions.*/
 	}
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
